a38b974163120115749dff2c171afc26dbf8da16
izu12ki <hirano.i@docodoor.co.jp>
2026-05-20 16:51:01 +0900

    feat(mock): モックデータ層 (users/chats/messages) の追加

---
+++ web/src/lib/mock/index.ts
@@ -0,0 +1,4 @@
+export * from './types';
+export { users, usersById } from './users';
+export { chats } from './chats';
+export { messages } from './messages';
---
+++ web/src/lib/mock/types.ts
@@ -0,0 +1,31 @@
+export type User = {
+  id: string;
+  nickname: string;
+  avatarUrl: string;
+  birthDate: string;
+  email: string;
+};
+
+export type Chat = {
+  id: string;
+  name: string;
+  iconUrl: string;
+  memberIds: string[];
+  lastMessageAt: string;
+  unreadCount: number;
+  isArchived: boolean;
+};
+
+export type MessageType = 'text' | 'image' | 'video';
+
+export type Message = {
+  id: string;
+  chatId: string;
+  senderId: string;
+  createdAt: string;
+  type: MessageType;
+  body: string;
+  thumbnailUrl?: string;
+};
+
+export const CURRENT_USER_ID = 'u-me';
